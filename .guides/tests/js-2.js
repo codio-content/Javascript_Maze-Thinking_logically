706eeb19695dd51ce6e8f15b288ea1c2ae8faf0f
SeanPackham <sean@seanpackham.com>
2015-07-31 10:21:43 +0000

    removed old test

--- .guides/tests/js-2.js
+++ .guides/tests/js-2.js
@@ -36,24 +36,3 @@ catch(e) {
 
 process.stdout.write('Not quite right, try again!');  
 process.exit(1);
-
-
-
-
-
-
-$.getScript(window.location.origin + '/public/content/js/' + window.testEnv.cmd + '.js')
-.done(function (script, status) {
-  console.log(_commands);
-  
-  if(indexOfCommand('createEmptyMaze') > -1 && indexOfCommand('addRandomMonsters') > -1 && command('addRandomMonsters')[1] == 3) {    
-    codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.SUCCESS, 'Well done!');
-  }
-  else {
-    codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.FAILURE, 'Not quite right, try again!');
-  }
-})
-.fail(function (jqxhr, settings, exception) {
-  console.log(exception);
-  codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.INVALID, exception.message); 
-});
